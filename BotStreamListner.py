de4adb7ccd440568ab05bd06bff9fbb0a46dae8a
hana <hana.ben-arab@polymtl.ca>
2017-01-21 14:59:33 -0500

    some changes

--- BotStreamListner.py
+++ BotStreamListner.py
@@ -5,3 +5,24 @@ class BotStreamListner(tweepy.StreamListener):
 	def on_error(Self,status_code):
 		if status_code ==420:
 			return False
+	def on_data(self, data):
+		print(data)
+		decoded = json.loads(data)
+		# listen only for tweets that is geo-location enabled
+		print('heyy')
+		try: 
+			tweet = {}
+			tweet['screen_name'] = '@'+decoded['user']['screen_name']
+			tweet['text'] = decoded['text'].encode('ascii', 'ignore')
+			if decoded['coordinates']:
+				tweet['coordinates'] = decoded['coordinates']['coordinates']
+				tweet['created_at'] = decoded['created_at']
+				print('A tweet received')
+				print(tweet)
+				# publish to 'tweet_stream' channel
+				return True
+			else:
+				pass
+
+		except:
+			pass
--- twitterBot.py
+++ twitterBot.py
@@ -7,6 +7,8 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-botStream = BotStreamListner();
+botStream = BotStreamListner()
 myStream = tweepy.Stream(auth= api.auth,listener = botStream)
-myStream.filter(track=['#askPY'],async=True)
+print('aa')
+myStream.filter(track=['#askYP'],async=True)
+print('bb')
