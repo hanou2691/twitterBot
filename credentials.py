43f55cce23d5b32460ddefa75efe01cbb2776829
Abderrahmane Laribi <abderrahmane.laribi@polymtl.ca>
2017-01-21 11:19:13 -0500

    Add credentials

---
+++ credentials.py
@@ -0,0 +1,4 @@
+CONSUMER_KEY = 'kberpt0Uf1UDcd15PhdfnIojK'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = 'dvvFhy0lQi10XZqTksWOQz1f88RrjeDfLoSsu5qhqggg7o2ZXZ'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '	813163906889818116-vOFeVKZzUW8VgFFdYgNuCKBX3JErxBV'#keep the quotes, replace this with your access token
+ACCESS_SECRET = 'VMj1bZNlqnsxoYMh3NDctxHpyUvGMWKkm2To2F4vNdxoL'#keep the quotes, replace this with your access token secret
