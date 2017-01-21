8ebc52b8efe327ceb1ed329f330da0a2f1b20437
Abderrahmane Laribi <abderrahmane.laribi@polymtl.ca>
2017-01-21 15:00:35 -0500

    New auth method

--- twitterBot.py
+++ twitterBot.py
@@ -4,9 +4,15 @@ from BotStreamListner import *
  
 # Access and authorize Twitter credentials
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+# Redirect user to Twitter to authorize
+redirect_user(auth.get_authorization_url())
+
+# Get access token
+auth.get_access_token("verifier_value")
+
+# Construct the API instance
 api = tweepy.API(auth)
 
-botStream = BotStreamListner();
+botStream = BotStreamListner()
 myStream = tweepy.Stream(auth= api.auth,listener = botStream)
 myStream.filter(track=['#askPY'],async=True)
